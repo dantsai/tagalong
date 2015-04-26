bae0e0fc7841e34c0e62baf54588e370aece1bfc
Pablo <parvizu@ischool.berkeley.edu>
2015-04-26 00:51:17 -0700

    Pill menu working for Activiy List and minor sorting/filter improvement to list

--- app/client/templates/activites/activity_list.js
+++ app/client/templates/activites/activity_list.js
@@ -1,4 +1,70 @@
 Template.activityList.helpers({
+	activityDisplay: function() {
+
+		var filter = Session.get('activityFilter');
+		var activities;
+		now = new Date();
+		date_now = now.setSeconds(0);
+
+		console.log(filter);
+		if (filter =='All') {
+			activities = Activities.find(
+				{ 
+					'available': true,
+				  	'time.date' : 
+						{ $gte: new Date(date_now) } 
+				},						 
+				{ 
+					sort : { 
+						'time.date': 1, 
+						'time.time': 1
+					} 
+				}					
+			);
+		}
+		else if (filter == 'New') {
+			activities = Activities.find(
+				{ 
+					'available': true,
+				  	'time.date' : { $gte: new Date(date_now) },
+					'host._id' : {$ne: Meteor.userId()}
+				},						 
+				{ 
+					sort : { 
+						'time.date': 1, 
+						'time.time': 1
+					} 
+				},
+				{ $limit : 4 }
+			); 
+		}
+		else {
+			if (Meteor.user().activities.length) {
+				activities = Activities.find(
+					{ 
+						'available': true,
+					  	'time.date' : 
+							{ $gte: new Date(date_now) },
+					  	'type': 
+					  		{ $in : Meteor.user().activities },
+						'host._id' : {$ne: Meteor.userId()} 
+					},						 
+					{ 
+						sort : { 
+							'time.date': 1, 
+							'time.time': 1
+						} 
+					}
+				);
+			}
+			else {
+				// Could return those with a lot of users.
+				//return 'Set Prefs'
+			}
+
+		}
+		return groupActivities(activities);
+	},
 	activitiesAll: function() {
 		now = new Date();
 		date_now = now.setSeconds(0);
@@ -71,6 +137,20 @@ Template.activityList.helpers({
 	}
 });
 
+Template.activityList.events({
+	'click .pillMenu li': function(event) {	
+		var selection = $(event.currentTarget);	
+		// console.log(selection);
+		if(!selection.hasClass('selected')) {
+			$(".pillMenu li").removeClass('selected');	
+			selection.toggleClass('selected');
+			Session.set('activityFilter',selection.text());
+		}
+	}
+})
+
+Session.set('activityFilter','Recommended');
+
 function groupActivities(activities) {
 	var grouped_obj = {}
 	
@@ -92,6 +172,6 @@ function groupActivities(activities) {
 	    return [value];
 	});
 
-	console.log(grouped_activities);
+	// console.log(grouped_activities);
 	return grouped_activities	
 }
