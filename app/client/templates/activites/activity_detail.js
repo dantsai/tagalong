1bd1f17e659351dcedbed28f4e3606bed7a68328
dan tsai <tsai.dan@gmail.com>
2015-05-02 20:22:06 -0700

    activityID not working, or messageType

--- app/client/templates/activites/activity_detail.js
+++ app/client/templates/activites/activity_detail.js
@@ -187,18 +187,113 @@ function recordVideo(activityId) {
 	console.log(Meteor.userId());
 
 	//ADD CODE FOR RECORDING VIDEO HERE.
+
+	// just as a test of camera functionality
+
+	// alert('camera...')
+	console.log('camera...')
+	console.log(this);
+	console.log(Meteor.userId());
+	// success callback
+    function captureSuccess(mediaFiles) {
+        var i, len, path;
+        for (i = 0, len = mediaFiles.length; i < len; i += 1) {
+            path = mediaFiles[i].fullPath;
+            alert(path);
+            uploadFile(mediaFiles[i]);
+        }
+    }
+
+	// capture error callback
+	function captureError(error) {
+	    console.log('Error code: ' + error.code, null, 'Capture Error');
+	    navigator.notification.alert('Error code: ' + error.code, null, 'Capture Error');
+	};
+
+    // function : Upload files to server
+    function uploadFile(mediaFile) {
+        var ft = new FileTransfer(),
+            path = mediaFile.fullPath,
+            name = mediaFile.name;
+        var options = new FileUploadOptions();
+        options.fileName=name;
+        options.mimeType= 'video/quicktime';
+
+
+        ft.upload(path,
+            "http://dantsai.com/_/upload.php",
+            function(result) {
+                console.log('Upload success: ' + result.responseCode);
+                console.log(result.bytesSent + ' bytes sent');
+                console.log('Upload success: ' + result.responseCode);
+                console.log('response: ' + result.response); // url of video. SAVE THIS
+                console.log(result.bytesSent + ' bytes sent');
+
+	    		// do we need to include a flag indicating if the "message to self" is a video or photo?
+                message = {'activity_id': this._id,
+                			'user': Meteor.userId(),
+                			'messageUrl' : result.response
+            				}
+
+                var msgId = Messages.insert(message);
+                //place to store the url for video.
+            },
+            function(error) {
+                alert('Error uploading file ' + path + ': ' + error.code + '. source: ' + error.source + '. target: ' + error.target);
+            },
+            { fileName: name });   
+    }
+
+	// start video capture
+	navigator.device.capture.captureVideo(captureSuccess, captureError, {limit:1});
 }
 
 function takePhoto(activityId) {
 	//passing the activity as a parameter so you can access the id.
 
 	// alert('camera...')
-	console.log('Photo...')
+	console.log('Take photo...')
 	console.log(activityId);
 	console.log(Meteor.userId());
 
 	//ADD CODE FOR TAKING PHOTO HERE.
-  
+	navigator.camera.getPicture(uploadPhoto,
+        function(message) { console.log('get picture failed'); },
+        { quality: 50, 
+        destinationType: navigator.camera.DestinationType.FILE_URI,
+        sourceType: navigator.camera.PictureSourceType.CAMERA }
+        );
+
+	function win(r) {
+	    // console.log("Code = " + r.responseCode);
+	    console.log("Response = " + r.response);
+	    // console.log("Sent = " + r.bytesSent);
+	    // console.log(r.response);
+
+	    // do we need to include a flag indicating if the "message to self" is a video or photo?
+        message = {'activity_id': this._id,
+        			'user': Meteor.userId(),
+        			'messageUrl' : r.response
+    				}
+
+        var msgId = Messages.insert(message);
+	}
+
+	function fail(error) {
+	    console.log("An error has occurred: Code = " + error.code + '. source: ' + error.source + '. target: ' + error.target);
+	    console.log("upload error source " + error.source);
+	    console.log("upload error target " + error.target);
+	}
+
+    function uploadPhoto(imageURI) {
+        var options = new FileUploadOptions();
+        options.fileKey="file";
+        options.fileName=imageURI.substr(imageURI.lastIndexOf('/')+1);
+        options.mimeType="image/jpeg";
+
+        var ft = new FileTransfer();
+        ft.upload(imageURI, encodeURI("http://dantsai.com/_/upload.php"), win, fail, options, true);
+    }
 }
 
 Meteor.startup(function () {
